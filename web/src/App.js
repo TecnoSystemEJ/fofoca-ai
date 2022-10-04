5e1edcdb8faf4db2fee80d07e234ddd08ce51d48
Luis Henrique da Silva Carvalho <luisdev9225@gmail.com>
2022-10-04 14:28:02 -0300

    :sparkles: feat: Pagina de login

--- web/src/App.js
+++ web/src/App.js
@@ -1,5 +1,19 @@
+import NavBar from "./components/NavBar";
+
+import { BrowserRouter as Router, Route, Routes, } from 'react-router-dom'
+
+
 function App() {
-  return <div className="App"></div>;
+  return <div className="App">
+    
+    <Router>
+      <NavBar/>
+      <Routes>
+        <Route></Route>
+      </Routes>
+    </Router>
+    
+  </div>;
 }
 
 export default App;
